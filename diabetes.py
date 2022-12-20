99f974f36bb3b490ff0faf88404805c53b950282
jaimegarcia2679 <61202759+jaimegarcia2679@users.noreply.github.com>
2022-12-20 12:40:18 -0500

    Add files via upload

--- diabetes.py
+++ diabetes.py
@@ -16,7 +16,7 @@ print("Modelo Cargado")
 
 st.title('Status Diabetes')
 st.header("Consulte su glucosa")
-st.subheader("Ingrese los datos para su verficación")
+st.subheader("Ingrese los datos para su verificación")
 
 
 with st.form(key='diabetes-pred-form'):
@@ -29,10 +29,18 @@ with st.form(key='diabetes-pred-form'):
 
 
     col5, col6, col7, col8= st.columns(4)
-    Insulina= col5.slider(label='Valor de Insulina:', min_value=20, max_value=400)
+    insulina= col5.slider(label='Valor de Insulina:', min_value=20, max_value=400)
     BMI = col6.slider(label='Ingrese valor IMC:', min_value=50, max_value=400)
     diabetes_hereditaria= col7.text_input(label='Ingrese su ascendencia:')
     age= col8.text_input(label='Ingrese su edad:')
 
     submit = st.form_submit_button(label='Analizar')
+    
+    calculo = pd.DataFrame({'Pregnancies':embarazos, 'PlasmaGlucose':glucosa, 'DiastolicBloodPressure':diastolica, 
+                            'TricepsThickness':grosor_piel, 'SerumInsulin':insulina, 'BMI':BMI,
+                            'DiabetesPedigree':diabetes_hereditaria, 
+                            'Age':age},index=[0])
+    predicted_diabetes = loaded_model.predict(calculo)[0]
+    print(predicted_diabetes)
+    
 
